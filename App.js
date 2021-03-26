a30cf36f7c81f4be811f4a2f3cf815c2b345983b
thiago henrique fragata <thiago.henrique.fragata@hotmail.com>
2021-03-26 06:46:08 -0400

    app concluido

--- App.js
+++ App.js
@@ -1,12 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
-export default function App() {
+import Slider from "@react-native-community/slider";
+import Clipboard from "expo-clipboard";
+
+let charset =
+  "0123456789!@#$%&*abcdfghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+export default function app() {
+  const [password, setPassword] = useState("");
+  const [size, setSize] = useState(8);
+
+  function generatePass() {
+    let pass = "";
+
+    for (let i = 0, n = charset.length; i < size; i++) {
+      pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    setPassword(pass);
+  }
+
+  function copyPass() {
+    Clipboard.setString(password);
+    alert("Senha copiada!");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./src/assets/logo.png")} style={styles.logo} />
+
+      <Text style={styles.title}>{size} caracteteres</Text>
+
+      <View style={styles.area}>
+        <Slider
+          style={{ height: 50 }}
+          minimumValue={5}
+          maximumValue={15}
+          minimumTrackTintColor="#ffa200"
+          maximumTrackTintColor="#000000"
+          value={size}
+          onValueChange={(valor) => setSize(valor.toFixed(0))}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={generatePass}>
+        <Text style={styles.buttonText}>Gerar senha</Text>
+      </TouchableOpacity>
+
+      {password !== "" && (
+        <View style={styles.area}>
+          <Text style={styles.password} onLongPress={copyPass}>
+            {password}
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -14,8 +62,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#f3f3ff",
+  },
+  logo: {
+    marginBottom: 60,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  area: {
+    marginTop: 15,
+    marginBottom: 15,
+    backgroundColor: "#ffffff",
+    width: "70%",
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: "#ffa200",
+    width: "70%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 7,
+    marginBottom: 25,
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#ffffff",
+  },
+  password: {
+    padding: 10,
+    textAlign: "center",
+    fontSize: 20,
   },
 });
---
+++ README.md
@@ -0,0 +1,76 @@
+# Gerador de senhas em React Native
+
+## ✅ Descrição do Projeto
+
+<p> Este projeto foi construído no Insider 2.0, workshop gratuido realizado pelo Matheus do <a href="https://sujeitoprogramador.com/"> Sujeito Programador</a>, com foco no desenvolvimento em <a href="https://reactnative.dev/"> React Native</a>. </p>
+<p> O Evento ocorreu do dia 10 ao dia 13 de março de 2021. Foram desenvolvidos 2 aplicativos, um Gerador de senhas e um de Previsão do clima. O código aqui presente neste repositório é referente ao Gerador de senhas.</p>
+<p> O objetivo foi mostrar a base do desenvolvimento em React Native, conceitos e funcionálidades que se fazem presentes em toda base e estrutura da progrmação nessa tecnologia.</p>
+
+<p>Alguns conceitos e componentes que foram ultilizados são listados abaixo:</p>
+- View
+- Text
+- Image
+- Estilizações com StyleSheet
+- Slider
+- Hooks com useState
+- Renderização condicional
+
+## 🎁 Imagem de como ficou o aplicativo
+
+<h1 align="center">
+  <img alt="Gerador de senhas" title="#Insider2.0" src="Imagens/GeradorDeSenhas.jpg" width=300/>
+</h1>
+
+## 🎲 Pré-requisitos e como rodar a aplicação
+
+Siga esses passos para rodar o aplicativo pelo [Expo](https://expo.io/)
+
+### Pré-requisitos
+
+Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas:
+[Git](https://git-scm.com), [Node.js](https://nodejs.org/en/) e um editor de código, recomendo o [VSCode](https://code.visualstudio.com/).
+
+Baixe também o aplicativo [Expo Go](https://expo.io/client) em seu celular, pois é por ele que você poderá ver o aplicativo rodando.
+
+### Rodando e executando a aplicação
+
+#### - Clone este repositório
+
+- com git clone
+
+#### - Acesse a pasta do projeto no terminal/cmd
+
+- cd gerador
+
+#### - Instale as dependências, com npm ou yarn
+
+- npm install
+- yarn install
+
+### Rodando o projeto, com npm ou yarn
+
+- npm start
+- yarn start
+
+Após isso, irá abrir uma janela no seu navegador, aguarde aparecer um qrcode no canto inferior esquerdo.
+
+### Visualizando o aplicativo rodando em seu celular
+
+- Quando o qrcode aparecer na aba que abriu em seu navegador, abra o aplicativo Expo Go em seu celular e escaneie o qrcode.
+- Aguarde a barra de progreesso chegar a 100% e o aplicativo abrirá em seu celular igual amostrado na imagem acima.
+
+## Como ultilizar o aplicativo
+
+- Selecione o tamanho que a senha seja
+- Clique em "Gerar senha"
+- A senha irá aparecer abaixo do botao "Gerar senha"
+- Para copiar a senha, pressione em cima da senha que foi gerada.
+
+### 🛠 Tecnologias
+
+As seguintes ferramentas foram ultilizada na construção do projeto:
+
+- [Expo](https://expo.io/)
+- [React Native](https://reactnative.dev/)
+
+Feito com ❤️+☕ por Thiago Henrique Fragata 👋👋 Entre em contato!🚀
